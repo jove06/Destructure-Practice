0bae1ffafeae895899861842d0ea9906b161f0a7
jove06 <alexiariccardi@gmail.com>
2025-09-14 13:28:17 -0400

    destructure a function

--- destructurePractice.js
+++ destructurePractice.js
@@ -1,4 +1,11 @@
+//Destructuring Arrays
 const names = ['John', 'Jacob', 'Jingleheimer'];
 const [firstName, secondName] = names;
 console.log(`${firstName} ${secondName}`);
 // => John Jacob
+
+//Destructuring Functions
+const foo = () => [1, 2, 3];
+const [one, two] = foo();
+console.log(two, one);
+// => 1 2
